11cd8796ca53bca8e66bad8035dc371bdec34371
Ayra Hikari <AyraHikari@gmail.com>
2020-10-02 18:54:35 +0700

    Added Help command

--- bot.py
+++ bot.py
@@ -11,4 +11,11 @@ bot = Client("nana", api_id=api_id, api_hash=api_hash, bot_token=SETTINGS_BOT_TO
 async def start(client, message):
 	await message.reply("Start successfully!")
 
+@bot.on_message(filters.command(["help"]))
+async def help(client, message):
+	await message.reply("Here is a tips from me @AyraHikari")
+	await message.reply("You can add command list at @BotFather")
+	await message.reply("With add /setting or /help will bring new menu called 'Settings' or 'Help' in bot PM")
+	await message.reply("Give a try!")
+
 bot.run()
